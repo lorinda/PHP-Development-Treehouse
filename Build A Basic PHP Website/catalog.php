32f7bf3283bd241d64e27bafe13aed92cda89d4f
Lorinda <lorinda@users.noreply.github.com>
2016-03-10 16:39:19 -0500

    Added data.php and foreach in catalog.php

--- Build A Basic PHP Website/catalog.php
+++ Build A Basic PHP Website/catalog.php
@@ -1,10 +1,6 @@
 <?php 
 
-$catalog = array();
-$catalog[101] = "Design Patterns";
-$catalog[201] = "Forrest Gump";
-$catalog[301] = "Beethoven";
-$catalog[102] = "Clean Code";
+include("includes/data.php");
 
 $pageTitle = "Full Catalog";
 
@@ -31,10 +27,14 @@ include("includes/header.php");?>
 <div class="section catalog page">
     <div class="wrapper">
         <h1><?php echo $pageTitle; ?></h1>
-        <ul>
+        <ul class="items">
             <?php 
             foreach($catalog as $item){
-                echo "<li>".$item."</li>";
+                echo "<li><a href='#'><img src='"
+                    .$item["img"]."'alt='"
+                    .$item["title"]. "' />"
+                    ."<p>View Details</p>"
+                    ."</a></li>";
             }
             ?>
         </ul>
